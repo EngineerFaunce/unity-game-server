9ea4ae97f73966338045d31803a1f2c965139cad
William Faunce <EngineerFaunce@users.noreply.github.com>
2021-01-29 13:58:27 -0500

    Initial commit

---
+++ GameServer/GameServer/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GameServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Game Server";
+
+            Server.Start(50, 20521);
+
+            Console.ReadKey();
+        }
+    }
+}
